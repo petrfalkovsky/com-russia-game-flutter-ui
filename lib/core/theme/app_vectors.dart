3f56145100f1719fef4ff1ec43add1a413fbef06
Peter Parker <petrfalkovsky@yandex.ru>
2025-07-14 03:07:15 +0300

    feat: add car plate assets

--- lib/core/theme/app_vectors.dart
+++ lib/core/theme/app_vectors.dart
@@ -6,4 +6,14 @@ class AppVectors {
   static const String parkingRepair = '$_parking/parking_repair.svg';
   static const String parkingSearch = '$_parking/parking_search.svg';
   static const String parkingCarPlatesRus = '$_parking/car_plates_rus.svg';
+
+  // Car plates
+  static const String _plates = 'assets/svg/game/car_plates';
+  static const String plateBelarus = '$_plates/plate_belarus.svg';
+  static const String plateBrazil = '$_plates/plate_brazil.svg';
+  static const String plateEmptyBrasil = '$_plates/plate_empty_brasil.svg';
+  static const String plateEmptyRus = '$_plates/plate_empty_rus.svg';
+  static const String plateKazakhstan = '$_plates/plate_kazakhstan.svg';
+  static const String plateRus = '$_plates/plate_rus.svg';
+  static const String plateUkrain = '$_plates/plate_ukrain.svg';
 }
